d3c854030b03f5d6e115b1534016da6bf191b89e
stefansalt <stefan111@foxmail.com>
2024-10-31 19:04:26 +0800

    feat(system): 新增班级管理功能
    - 创建 Clazz 实体类，用于表示班级信息
    - 创建 ClazzBO 类，用于班级相关的业务操作
    - 新增 ClazzMapper.xml 文件，定义班级相关的数据库操作
    - 实现班级分页列表查询和全部班级列表查询功能

---
+++ src/main/java/com/lee/selection/system/model/entity/Clazz.java
@@ -0,0 +1,46 @@
+package com.lee.selection.system.model.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 班级 院系  专业 可以通过树结构一次处理实体
+ *
+ * @author baomidou
+ * @since 2024-10-30
+ */
+@Getter
+@Setter
+public class Clazz implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * id
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 所属专业id
+     */
+    private Integer parentId;
+
+    /**
+     * 班级描述
+     */
+    private String description;
+
+    /**
+     * 班级名称
+     */
+    private String name;
+
+    /**
+     * 班主任
+     */
+    private Integer teacherId;
+}
